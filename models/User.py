22bbd93878752d9a5ce05c24124a06a2a0fe77de
Ghulam Abbas <ghulam_abbas_baber@outlook.com>
2024-10-21 13:55:45 +0500

    add migrations and define user table

---
+++ models/User.py
@@ -0,0 +1,21 @@
+# models.py
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy_utils import URLType
+from db.database import Base  # Import the Base from the db module
+from datetime import datetime, timedelta
+from sqlalchemy.orm import relationship
+
+# Example model
+class User(Base):
+    __tablename__ = 'user_table'
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    phone = Column(String(50))
+    email = Column(String(100),unique=True)
+    address = Column(String(225))
+    password = Column(String(225))
+    role = Column(String(50))
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=None, onupdate=datetime.utcnow)
